bd177e5af60458023978b9cdd424ed7be298d849
sshahram <shirin.shahram23@gmail.com>
2021-04-04 21:17:05 -0700

    modify role for Manager, Engineer & Intern classes

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -11,3 +11,9 @@ test("get Intern' school name", () => {
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school));
 });
+
+test("get Intern's role", () => {
+    const intern = new Intern('Jack', 789, 'jack@gmail.com', 'UC Berkeley');
+
+    expect(intern.getRole()).toEqual("Intern");
+})

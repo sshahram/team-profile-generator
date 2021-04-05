2448bd4ea8d69e33ba6bced08081c20fc7c50fcf
sshahram <shirin.shahram23@gmail.com>
2021-04-04 17:57:39 -0700

    modify role property for Employee & Manager classes

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,13 @@
 const Manager = require('../lib/Manager');
 
-test("get Manager's office number", () => {
+test("check Manager's office number", () => {
     const manager = new Manager('David', 567, 'david@gmail.com', 2);
 
     expect(manager.officeNumber).toBe(2);
+});
+
+test("get Manager's role", () => {
+    const manager = new Manager('David', 567, 'david@gmail.com', 2);
+
+    expect(manager.getRole()).toEqual("Manager");
 });

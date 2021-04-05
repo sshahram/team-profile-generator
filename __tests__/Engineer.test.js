acff982589c4e44ee236b925727d955d066d8140
sshahram <shirin.shahram23@gmail.com>
2021-04-04 20:19:47 -0700

    add getGithub() to Engineer class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,3 +5,9 @@ test("check Engineer's github username", () => {
 
     expect(engineer.github).toBe('sshahram');
 });
+
+test("get Engineer's GitHub username", () => {
+    const engineer = new Engineer('Shirin', 567, 'shirin@gmail.com', 'sshahram');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github));
+});

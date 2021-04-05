5d8c5387501b97f9d86aa9034c7cefb7efa571e8
sshahram <shirin.shahram23@gmail.com>
2021-04-04 17:49:02 -0700

    add officeNumber property for Manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,7 @@
 const Manager = require('../lib/Manager');
 
+test("get Manager's office number", () => {
+    const manager = new Manager('David', 567, 'david@gmail.com', 2);
 
+    expect(manager.officeNumber).toBe(2);
+});

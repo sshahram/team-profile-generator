08f2ffaf7879faed73187763520c9ba0f519270b
sshahram <shirin.shahram23@gmail.com>
2021-04-04 21:12:38 -0700

    add getSchool() to Intern class

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,13 @@
+const Intern = require('../lib/Intern');
+
+test("check Intern's school", () => {
+    const intern = new Intern('Jack', 789, 'jack@gmail.com', 'UC Berkeley');
+
+    expect(intern.school).toBe('UC Berkeley');
+});
+
+test("get Intern' school name", () => {
+    const intern = new Intern('Jack', 789, 'jack@gmail.com', 'UC Berkeley');
+    
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school));
+});
